20f9638d5cf0d3f708ba8b6473e12d88306acc62
Treasure Porth <treasure.rose@gmail.com>
2016-12-17 20:12:52 -0800

    All required info displayed, no styling

--- app.js
+++ app.js
@@ -7,30 +7,56 @@ var twit = new Twit(config);
 app.set('view engine', 'pug');
 
 twit.get('statuses/user_timeline', {screen_name: "treasureporth", count: 5}, function(err,data,res){
+  var userInfo;
   var tweetList = [];
   var userList = [];
   var messageList = [];
 
   data.forEach(function(tweet){
-    tweetList.push(tweet.text);
+    userInfo = {
+      username : tweet.user.name,
+      screenname : tweet.user.screen_name,
+      profileImg : tweet.user.profile_image_url,
+    }
+
+    tweetList.push({
+      text : tweet.text,
+      favorite_count: tweet.favorite_count,
+      retweet_count: tweet.retweet_count,
+      createdAt: tweet.created_at
+    });
   });
 
   twit.get('friends/list', {screen_name: "treasureporth", count: 5}, function(err,data,res){
     var users = data.users;
-
     users.forEach(function(user){
-        userList.push(user.name);
+        userList.push({
+          screenname: user.screen_name,
+          name: user.name,
+          img: user.profile_image_url,
+          following: user.following
+        });
     });
   });
 
   twit.get('direct_messages', {screen_name: "treasureporth", count: 5}, function(err,data,res){
     data.forEach(function(message){
-      messageList.push(message.text);
+      messageList.push({
+        sender: message.sender.name,
+        message: message.text,
+        sender_screename: message.sender_screen_name
+      });
     });
   });
 
   app.get('/', function(req,res,err){
-    res.render('index', {title: "Super Duper Twitter App", tweetList: tweetList, userList: userList, messageList: messageList})
+    res.render('index', {
+      title: "Super Duper Twitter App",
+      tweetList: tweetList,
+      userInfo: userInfo,
+      userList: userList,
+      messageList: messageList
+    });
   });
 });
 
